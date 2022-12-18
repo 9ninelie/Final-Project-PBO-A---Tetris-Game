2cfed24f0c9cf6b266f6e485f889378f18b569be
9ninelie <kenkazuki99@gmail.com>
2022-12-18 18:11:28 +0700

    Creator FXML Controller

---
+++ src/main/tampilInfoController.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class tampilInfoController {
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
+    void back(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/tampilMenu.fxml"));
+            Scene scene = new Scene(root);
+            Main.getStage().setScene(scene);
+            Main.getStage().setTitle("MENU ONG TETRIS");
+        } catch (IOException e) {
+            System.out.println("tidak bisa memuat halaman");
+        }
+    }
+
+}
+
+
